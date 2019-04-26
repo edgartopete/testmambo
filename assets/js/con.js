7dcccac70569279075a10ebae4b6feb2703144d2
et code <edgar.topete@gmail.com>
2019-04-26 14:04:49 -0500

    v1

---
+++ assets/js/con.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBEJRfBSbC2vnJu7T-GLa8B-X3wMcr4hTg",
+    authDomain: "mambo-ab916.firebaseapp.com",
+    databaseURL: "https://mambo-ab916.firebaseio.com",
+    projectId: "mambo-ab916",
+    storageBucket: "mambo-ab916.appspot.com",
+    messagingSenderId: "978683882765"
+  };
+  firebase.initializeApp(config);
+
+
